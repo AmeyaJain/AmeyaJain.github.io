df8ff3787a55be8808e9c6bcab2dbef7913ce789
AmeyaJain <allwell1225@gmail.com>
2020-08-11 00:13:45 +0530

    Jain to Jim Done

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -16,7 +16,7 @@ Good Bye Jim
 */
 
 (function() {
-    var names = ["Yaakov", "John", "James", "Javed", "Jain", "Ameya", "Coursera", "JavaScript", "Laura", "Jack"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
